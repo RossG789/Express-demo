b4e9b44627d7e7f952c069cf96c5a11469554aa0
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-29 14:34:17 +0000

    first of many

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
+
+app.get("/messages", (req, res) => {
+  console.log(res);
+  res.json({ message: "Hello, World!" });
+});
